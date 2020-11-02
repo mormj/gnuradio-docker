52ab10f5bc0f36f1f369b4ccdf3174eabb324f19
mormj <mormjb@gmail.com>
2020-11-02 08:05:06 -0500

    update start docker to vary image name

--- startDocker.sh
+++ startDocker.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+
+if [ "$3" = "base" ]; then
+    NAME="gnuradio"
+    echo "${NAME}_${1}_${2}_${3}"
+else
+    NAME=gnuradio-master
+    echo "${NAME}_${1}_${2}_${3}"
+fi
+
+
 xhost +
 docker run --cpus 4 -it \
                -v ~/.Xauthority:/root/.Xauthority \
                -v /tmp/.X11-unix/:/tmp/.X11-unix:rw \
                -e DISPLAY=$DISPLAY -e QT_X11_NO_MITSHM=1 \
-                gnuradio-master_$1_$2_$3 "bash"
+               "${NAME}_${1}_${2}_${3}" "bash"
             #    -v /share/tmp:/data \
             #    -p 5678:5679 \
